f4b099dd9e393aee6ce67aa678f64053d75fcd51
zaitkulov <zaitkulov@gmail.com>
2024-01-14 04:37:39 +0500

    Storyboard deleted

--- HomeWork/SceneDelegate.swift
+++ HomeWork/SceneDelegate.swift
@@ -20,6 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let window = UIWindow(windowScene: windowScene)
         window.makeKeyAndVisible()
         self.window = window
+        self.window?.rootViewController = UINavigationController(rootViewController: ViewController())
         
     }
 
