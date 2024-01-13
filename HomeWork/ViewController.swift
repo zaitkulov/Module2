b355bbcd9d4de9735da6215471c37902257cc945
zaitkulov <zaitkulov@gmail.com>
2024-01-14 04:11:28 +0500

    Initial Commit

---
+++ HomeWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork
+//
+//  Created by Артем Заиткулов on 14.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
